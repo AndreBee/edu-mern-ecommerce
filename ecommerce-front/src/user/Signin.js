5ad5123fe6415d0cb5e8221c261243610e637f18
Andrea Baggio <abaggio@Andreas-MacBook-Air.local>
2022-04-04 09:03:00 +0400

    Completed user sign up component

--- ecommerce-front/src/user/Signin.js
+++ ecommerce-front/src/user/Signin.js
@@ -1,13 +1,86 @@
-import React from "react";
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
 import Layout from "../core/Layout";
+import { signin } from "../auth";
 
-const Signin = () => (
-    <Layout
-        title="Sign in"
-        description="Please sign in into your account."
-    >
-        ...
-    </Layout>
-)
+const Signin = () => {
+
+    const [values, setValues] = useState({
+        email: '',
+        password: '',
+        error: '',
+        loading: false
+    })
+
+    // destructuring values in the state
+    const {name, email, password, success, error} = values
+
+    const handleChange = name => event => {
+        setValues({...values, error: false, [name]: event.target.value})
+    }
+
+    const clickSubmit = (event) => {
+        event.preventDefault()
+        setValues({...values, error: false})
+        signin({email, password})
+            .then(data => {
+                if(data.error){
+                    setValues({...values, error: data.error, success: false})
+                } else {
+                    setValues({...values, name: '', email: '', password: '', error: '', success: true})
+                }
+            })
+    }
+
+
+    const signUpForm = () => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Email</label>
+                <input 
+                    onChange={handleChange('email')} 
+                    type="email" 
+                    className="form-control" 
+                    value={email}
+                />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Password</label>
+                <input 
+                    onChange={handleChange('password')} 
+                    type="password" 
+                    className="form-control" 
+                    value={password} 
+                />
+            </div>
+            <button onClick={clickSubmit} className="btn btn-primary">Submit</button>
+        </form>
+    )
+
+
+    const showError = () => (
+        <div className="alert alert-danger" style={{display: error ? '' : 'none'}}>
+            {error}
+        </div>
+    )
+
+    const showSuccess = () => (
+        <div className="alert alert-success" style={{display: success ? '' : 'none'}}>
+            New account is created. Please <Link to="/signin">sign in.</Link>
+        </div>
+    )
+
+    return (
+        <Layout
+        title="Sign up"
+        description="Please sign up into your account."
+        className="container col-md-8 offset-md-2"
+        >
+           {showSuccess()}
+           {showError()}
+           {signUpForm()}
+        </Layout>
+    )
+}
 
 export default Signin
--- ecommerce-front/src/user/Signup.js
+++ ecommerce-front/src/user/Signup.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
-import { API } from "../config";
+import { Link } from "react-router-dom";
 import Layout from "../core/Layout";
+import { signup } from "../auth";
 
 const Signup = () => {
 
@@ -13,33 +14,23 @@ const Signup = () => {
     })
 
     // destructuring values in the state
-    const {name, email, password} = values
+    const {name, email, password, success, error} = values
 
     const handleChange = name => event => {
         setValues({...values, error: false, [name]: event.target.value})
     }
 
-    const signup = (user) => {
-        // send data to backend
-        fetch(`${API}/signup`, {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                "Content-Type": 'application/json'
-            },
-            body: JSON.stringify(user)
-        })
-        .then(response => {
-            return response.json()
-        })
-        .catch(err => {
-            console.log(err)
-        })
-    }
-
     const clickSubmit = (event) => {
         event.preventDefault()
+        setValues({...values, error: false})
         signup({name, email, password})
+            .then(data => {
+                if(data.error){
+                    setValues({...values, error: data.error, success: false})
+                } else {
+                    setValues({...values, name: '', email: '', password: '', error: '', success: true})
+                }
+            })
     }
 
 
@@ -47,18 +38,46 @@ const Signup = () => {
         <form>
             <div className="form-group">
                 <label className="text-muted">Name</label>
-                <input onChange={handleChange('name')} type="text" className="form-control" />
+                <input 
+                    onChange={handleChange('name')} 
+                    type="text" 
+                    className="form-control" 
+                    value={name}
+                />
             </div>
             <div className="form-group">
                 <label className="text-muted">Email</label>
-                <input onChange={handleChange('email')} type="email" className="form-control" />
+                <input 
+                    onChange={handleChange('email')} 
+                    type="email" 
+                    className="form-control" 
+                    value={email}
+                />
             </div>
             <div className="form-group">
                 <label className="text-muted">Password</label>
-                <input onChange={handleChange('password')} type="password" className="form-control" />
+                <input 
+                    onChange={handleChange('password')} 
+                    type="password" 
+                    className="form-control" 
+                    value={password} 
+                />
             </div>
             <button onClick={clickSubmit} className="btn btn-primary">Submit</button>
         </form>
+    )
+
+
+    const showError = () => (
+        <div className="alert alert-danger" style={{display: error ? '' : 'none'}}>
+            {error}
+        </div>
+    )
+
+    const showSuccess = () => (
+        <div className="alert alert-success" style={{display: success ? '' : 'none'}}>
+            New account is created. Please <Link to="/signin">sign in.</Link>
+        </div>
     )
 
     return (
@@ -67,8 +86,9 @@ const Signup = () => {
         description="Please sign up into your account."
         className="container col-md-8 offset-md-2"
         >
+           {showSuccess()}
+           {showError()}
            {signUpForm()}
-           {JSON.stringify(values)}
         </Layout>
     )
 }
