12a87f856a2dca4110de6f8f8330416bdaf3136c
Andrea Baggio <abaggio@Andreas-MacBook-Air.local>
2022-04-15 09:20:41 +0400

    Product cards with images

--- ecommerce-front/src/core/Home.js
+++ ecommerce-front/src/core/Home.js
@@ -1,13 +1,69 @@
 import React from "react";
 import Layout from "./Layout";
+import { getProducts } from "./apiCore";
+import { useEffect, useState } from "react";
+import Card from "./Card";
 
-const Home = () => (
-    <Layout
-        title="Home page"
-        description="Node React Ecomerce App"
-    >
-        ...
-    </Layout>
-)
+
+const Home = () => {
+
+    // state
+    const [productsBySell, setProductsBySell] = useState([])
+    const [productsByArrival, setProductsByArrival] = useState([])
+    const [error, setError] = useState(false)
+
+    // functions
+    const loadProductsBySell = () => {
+        getProducts('sold')
+        .then(data => {
+            if(data.error){
+                setError(data.error)
+            } else {
+                setProductsBySell(data)
+            }
+        })
+    }
+
+    const loadProductsByArrival = () => {
+        getProducts('createdAt')
+        .then(data => {
+            if(data.error){
+                setError(data.error)
+            } else {
+                setProductsByArrival(data)
+            }
+        })
+    }
+
+
+    useEffect(() => {
+        loadProductsByArrival()
+        loadProductsBySell()
+    }, [])
+
+    return (
+        <Layout
+            title="Home page"
+            description="Node React Ecomerce App"
+            className="container-fluid"
+        >
+            <h2 className="mb-4">New Arrivals</h2>
+            <div className="row">
+                {productsByArrival.map((product, index) => (
+                    <Card key={index} product={product}/>
+                ))}
+            </div>
+
+            <h2 className="mb-4">Best Sellers</h2>
+            <div className="row">
+                {productsBySell.map((product, index) => (
+                    <Card key={index} product={product}/>
+                ))}        
+            </div>     
+        </Layout>
+    )
+}
+
+
 
 export default Home
