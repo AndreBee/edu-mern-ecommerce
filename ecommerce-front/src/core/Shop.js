efc632e07783d2f4f949449e9683cb4544d4c3ba
Andrea Baggio <abaggio@Andreas-MacBook-Air.local>
2022-04-20 08:46:49 +0400

    Load more button for shop

--- ecommerce-front/src/core/Shop.js
+++ ecommerce-front/src/core/Shop.js
@@ -6,6 +6,7 @@ import { getCategories } from "./apiCore";
 import Checkbox from "./Checkbox";
 import RadioBox from "./RadioBox";
 import { prices } from "./FixedPrices";
+import { getFilteredProducts } from "./apiCore";
 
 
 const Shop  = () => {
@@ -18,6 +19,11 @@ const Shop  = () => {
             price: []
         }
     })
+    const [limit, setLimit] = useState(6)
+    const [skip, setSkip] = useState(0)
+    const [filteredResults, setFilteredResults] = useState([])
+
+    const [size, setSize] = useState(0)
 
     // load categories and set form data
     const init = () => {
@@ -30,8 +36,48 @@ const Shop  = () => {
         })
     }
 
+    const loadFilteredResults = newFilters => {
+        getFilteredProducts(skip, limit, newFilters)
+        .then(data => {
+            if(data.error){
+                setError(data.error)
+            } else {
+                setFilteredResults(data.data)
+                setSize(data.size)
+                setSkip(0)
+            }
+        })
+    }
+
+    const loadMore = () => {
+
+        let toSkip = skip + limit
+
+        getFilteredProducts(toSkip, limit, myFilters.filters)
+        .then(data => {
+            if(data.error){
+                setError(data.error)
+            } else {
+                setFilteredResults([...filteredResults, ...data.data])
+                setSize(data.size)
+                setSkip(toSkip)
+            }
+        })
+    }
+
+    const loadMoreBtn = () => {
+        return (
+            size > 0 && size >= limit && (
+                <button onClick={loadMore} className="btn btn-warning mb-5">
+                    Load more
+                </button>
+            )
+        )
+    }
+
     useEffect(() => {
         init()
+        loadFilteredResults(skip, limit, myFilters.filters)
     },[])
 
     const handleFilters = (filters, filterBy) => {
@@ -43,6 +89,7 @@ const Shop  = () => {
             let priceValues = handlePrice(filters)
             newFilters.filters[filterBy] = priceValues
         }
+        loadFilteredResults(myFilters.filters)
         setMyFilters(newFilters)
     }
 
@@ -82,7 +129,14 @@ const Shop  = () => {
                     </div>
                 </div>
                 <div className="col-8">
-                    {JSON.stringify(myFilters)}
+                    <h2 className="mb-4">Products</h2>
+                    <div className="row">
+                        {filteredResults.map((product, index)=> (
+                                <Card key={index} product={product} />
+                        ))}
+                    </div>
+                    <hr/>
+                    {loadMoreBtn()}
                 </div>
             </div>
         </Layout>
