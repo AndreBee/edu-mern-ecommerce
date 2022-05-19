53f6ffc679764dba680e8ad5922221f4a9010461
Andrea Baggio <abaggio@Andreas-MacBook-Air.local>
2022-05-19 08:44:54 +0400

    Update order status

--- ecommerce/controllers/order.js
+++ ecommerce/controllers/order.js
@@ -1,6 +1,23 @@
 const { Order, CartItem } = require('../models/order')
 const {errorHandler} = require('../helpers/dbErrorHandler')
 
+
+exports.orderById = (req, res, next, id) => {
+    // find order in DB
+    Order.findById(id)
+    .populate('products.product', 'name price')
+    .exec((err, order) => {
+        if(err || !order) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            })
+        }
+        // make order available in the request
+        req.order = order
+        next()
+    })
+}
+
 exports.create = (req, res) => {
     // add user from req.profile to order
     req.body.order.user = req.profile
@@ -31,4 +48,25 @@ exports.listOrders = (req, res) => {
 
         res.json(orders)
     })
+}
+
+
+exports.getStatusValues = (req, res) => {
+    res.json(Order.schema.path('status').enumValues)
+}
+
+
+exports.updateOrderStatus = (req, res) => {
+    Order.updateOne(
+        {_id: req.body.orderId}, 
+        {$set: {status: req.body.status}},
+        (err, order) => {
+            if(err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            res.json(order)
+        }
+    )
 }
